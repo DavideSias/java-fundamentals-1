d5f393e948c0b8886cf11c6242f3ca8d8eddf0e7
DavideSias <davidesias77@gmail.com>
2023-05-31 15:26:27 +0200

    Refactor FizzBuzz bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -9,17 +9,20 @@ public class FizzBuzz {
         System.out.println("Inserire un numero da 1 a 1000: ");
         int numberToFizzBuzz = myScanner.nextInt();
 
-        if (numberToFizzBuzz > 0 && numberToFizzBuzz <= 1000) {
-            for (int i = 1; i <= 100; i++) {
-                if ((i % 3 == 0) && (i % 5 == 0)) {
-                    System.out.println("FizzBuzz");
-                } else if (i % 3 == 0) {
-                    System.out.println("Fizz");
-                } else if (i % 5 == 0) {
-                    System.out.println("Buzz");
-                } else {
-                    System.out.println(i);
-                }
+        while (numberToFizzBuzz == 0 || numberToFizzBuzz > 1000) {
+            System.out.println("Numero non corretto! Inserire un numero da 1 a 1000: ");
+            numberToFizzBuzz = myScanner.nextInt();
+        }
+
+        for (int i = 1; i <= numberToFizzBuzz; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
         }
     }
